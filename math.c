c2f8b86434f0115e487058edc0381acc9bd1475f
epicprogramer <67761728+epicprogramer@users.noreply.github.com>
2020-07-16 14:09:38 +0200

    Update math.c

--- math.c
+++ math.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 double sum(double a, double b){
 	return a + b;
 }
@@ -12,4 +14,16 @@ double division(double a, double b){
 
 double mult(double a, double b){
 	return a * b;
+}
+
+void testMathOperations(){
+	double sumvar = sum(55, 21);
+	double multvar = mult(9,8);
+	double subvar = sub(22.7, 10.3);
+    double divvar = division(99, 900);
+
+	printf("%f\n", sumvar);
+    printf("%f\n", subvar );
+	printf("%f\n", divvar );
+	printf("%f\n", multvar );
 }
